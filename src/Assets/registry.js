5c6bcb22932e9b932b99fe8f0af3fd21d1959bd8
Claudia <claudiajoppa@gmail.com>
2024-07-12 18:40:07 +0000

    feat: added registry file

---
+++ src/Assets/registry.js
@@ -0,0 +1,23 @@
+const Asset = {
+    Brand: {
+      amazon: require('../Assets/Brands/amazon.png'),
+      handm: require('../Assets/Brands/handm.png'),
+      lacoste: require('../Assets/Brands/lacoste.png'),
+      levi: require('../Assets/Brands/levi.png'), 
+      shopify: require('../Assets/Brands/shopify.png'),   
+    },
+
+    Images: {},
+
+    Icons: {
+        facebook: require('../Assets/Icons/facebook.png'),
+        instagram: require('../Assets/Icons/instagram.png'),
+        twitter: require('../Assets/Icons/twitter.png'),
+        linkenin: require('../Assets/Icons/linkenin.png'),
+    },
+
+    Fonts: {
+        
+    },
+
+}
